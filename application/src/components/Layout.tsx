7eebea0471d1b15133d95ecb8cb0f242a29dd3fb
kazuki-lv12 <if.kazuki@gmail.com>
2022-09-08 17:01:22 +0900

    udpate tag list component

--- application/src/components/Layout.tsx
+++ application/src/components/Layout.tsx
@@ -1,16 +1,18 @@
 import { ReactNode } from 'react'
+import { List } from './modules/List'
 
 type Props = {
   contents: ReactNode
-  sideber: ReactNode
 }
 
-export const Layout = ({ contents, sideber }: Props) => {
+export const Layout = ({ contents }: Props) => {
   return (
     <div className="bg-[#F1F5F9] pt-16 min-h-[100vh]">
       <div className="flex justify-center">
         <div className="w-[900px]">{contents}</div>
-        <div className="ml-8">{sideber}</div>
+        <div className="ml-8">
+          <List />
+        </div>
       </div>
     </div>
   )
--- application/src/components/modules/List.tsx
+++ application/src/components/modules/List.tsx
@@ -1,18 +1,23 @@
+import { useGetTags } from 'config/microcms'
 import { Link } from 'react-router-dom'
 import { cssMyList } from '../my-style'
 
-const microcmsTags = ['none', 'HTML', 'CSS', 'JavaScript', 'Node.JS', 'React']
-
 export const List = () => {
+  const { tags } = useGetTags()
+
   return (
     <>
       <h3 className="text-center">tag list</h3>
       <div className="max-w-xs flex flex-col">
-        {microcmsTags.map((tag, index) => {
+        {tags.map((tag, index) => {
           return (
             <li key={index} className={`${cssMyList}`}>
-              <Link to={`/?tag=${tag}`} className="no-underline">
-                {tag}
+              <Link
+                to={`/?tag=${Object.keys(tag)}`}
+                className="flex no-underline"
+              >
+                <p className="m-0 w-20">{Object.keys(tag)}</p>
+                <div>{Object.values(tag)}</div>
               </Link>
             </li>
           )
